d94c1d99436639d42633ff79dba913dd5ba8a668
juan-aga <juan-aga@student.42malaga.com>
2023-10-15 20:00:48 +0200

    ADD: files from ex02.

---
+++ ex03/inc/ClapTrap.hpp
@@ -0,0 +1,35 @@
+#ifndef CLAPTRAP_H
+# define CLAPTRAP_H
+
+#include <iostream>
+
+#define CLAP_TRAP_HEALTH 10
+
+class ClapTrap
+{
+protected:
+	std::string		_name;
+	unsigned int	_hitPoints;
+	unsigned int	_energyPoints;
+	unsigned int	_attackDamage;
+
+public:
+	ClapTrap( void );
+	ClapTrap( std::string name );
+	ClapTrap( ClapTrap const & c );
+	~ClapTrap( void );
+
+	ClapTrap &	operator=( ClapTrap const & c );
+
+	void		attack( const std::string& target );
+	void		takeDamage( unsigned int amount );
+	void		beRepaired( unsigned int amount );
+
+	std::string	getName( void ) const;
+	void		getStatus( unsigned int stats[3] ) const;
+		
+};
+
+std::ostream &	operator<<( std::ostream & o, ClapTrap const & c );
+
+#endif
---
+++ ex03/src/ScavTrap.cpp
@@ -0,0 +1,60 @@
+#include "ScavTrap.hpp"
+
+#include <iostream>
+
+ScavTrap::ScavTrap( void )
+{
+	std::cout << "ScavTrap basic constructor called." << std::endl;
+	*this = ScavTrap("");
+}
+
+ScavTrap::ScavTrap( std::string name ) : ClapTrap(name)
+{
+	this->_hitPoints = SCAV_TRAP_HEALTH;
+	this->_energyPoints = 50;
+	this->_attackDamage= 20;
+
+	std::cout << "ScavTrap string constructor called." << std::endl;
+}
+
+ScavTrap::ScavTrap( ScavTrap const & s ) : ClapTrap(s)
+{
+	std::cout << "ScavTrap copy constructor called." << std::endl;
+}
+
+ScavTrap::~ScavTrap( void )
+{
+	std::cout << "ScavTrap destructor called." << std::endl;
+}
+
+void		ScavTrap::guardGate( void )
+{
+	std::cout << "ScavTrap " << this->_name << " is now in Gate keeper mode." << std::endl;
+}
+
+void			ScavTrap::attack( const std::string & target )
+{
+	unsigned int	stats[3];
+
+	this->getStatus(stats);
+	if (stats[0] == 0 || stats[1] == 0)
+		std::cout << this->_name << " can't attack!" << std::endl;
+	else
+	{
+		this->_energyPoints--;
+		std::cout << "ScavTrap " << this->_name << " attacks " << target;
+		std::cout << ", causing " << stats[2] << " points of damage!";
+		std::cout << std::endl;
+	}
+	if (DEBUG)
+		std::cout << *this;
+}
+
+ScavTrap &	ScavTrap::operator=( ScavTrap const & s )
+{
+	if (this != &s)
+	{
+		ClapTrap::operator=(s);
+	}
+	return *this;
+}
